81285d37fbfff4036c498b5c143b81789c705cfe
Tefy Andriambelonjatovo <tefy.nahary@gmail.com>
2019-01-28 22:22:41 +0300

    - Add map to desktop version

--- src/components/SearchPage/index.js
+++ src/components/SearchPage/index.js
@@ -1,16 +1,24 @@
-import React, {Component} from 'react';
-import {withStyles} from '@material-ui/core/styles';
-import {Query} from 'react-apollo';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { Query } from 'react-apollo';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import {RESTAURANT_SEARCH_QUERY} from '../../graphql/queries';
+import { RESTAURANT_SEARCH_QUERY } from '../../graphql/queries';
 import RestaurantCard from './RestaurantCard';
-
+import RestaurantMap from './RestaurantMap';
 
 const styles = {
   container: {
     backgroundColor: '#c5d1d7',
     height: '100%',
+    overflow: 'hidden',
+    display: 'flex'
+  },
+  restaurant: {
+    height: '100%',
     overflowY: 'auto'
+  },
+  map: {
+    flex: 1
   }
 };
 
@@ -23,7 +31,7 @@ class SearchPage extends Component {
   }
 
   render() {
-    const {address} = this.state;
+    const { address } = this.state;
 
     return (
       // Variables can be either lat and lon OR address
@@ -33,7 +41,7 @@ class SearchPage extends Component {
           address
         }}
       >
-        {({loading, error, data = {}}) => {
+        {({ loading, error, data = {} }) => {
           if (loading) {
             return <CircularProgress />;
           }
@@ -51,30 +59,42 @@ class SearchPage extends Component {
             const { classes } = this.props;
             return (
               <div className={classes.container}>
-                {data.search_restaurants.results.map((r, index) => {
-                  const {
-                    id, images = [], references = [], title = '',
-                    cuisine, open_closed: openClosed, rating,
-                    distance
-                  } = r;
-                  const reference = references[0] && references[0].site_name;
-                  const referencesCount = references.length;
-                  const featuredImage = (images && images.length && images[0]) || null;
-                  return (
-                    <RestaurantCard
-                      key={index}
-                      id={id}
-                      title={title}
-                      cuisine={cuisine}
-                      reference={reference}
-                      referencesCount={referencesCount}
-                      openClosed={openClosed}
-                      featuredImage={featuredImage}
-                      rating={rating}
-                      distance={distance}
-                    />
-                  );
-                })}
+                <div className={classes.restaurant}>
+                  {data.search_restaurants.results.map((r, index) => {
+                    const {
+                      id, images = [], references = [], title = '',
+                      cuisine, open_closed: openClosed, rating,
+                      distance
+                    } = r;
+                    const reference = references[0] && references[0].site_name;
+                    const referencesCount = references.length;
+                    const featuredImage = (images && images.length && images[0]) || null;
+                    return (
+                      <RestaurantCard
+                        key={index}
+                        id={id}
+                        title={title}
+                        cuisine={cuisine}
+                        reference={reference}
+                        referencesCount={referencesCount}
+                        openClosed={openClosed}
+                        featuredImage={featuredImage}
+                        rating={rating}
+                        distance={distance}
+                      />
+                    );
+                  })}
+                </div>
+                <div className={classes.map}>
+                  <RestaurantMap
+                    data={data.search_restaurants.results}
+                    isMarkerShown
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                    loadingElement={<div style={{ height: '100%' }} />}
+                    containerElement={<div style={{ height: '100%' }} />}
+                    mapElement={<div style={{ height: '100%' }} />}
+                  />
+                </div>
               </div>
             );
           }
